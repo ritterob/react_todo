f2152ca1b467c53291887f3ddff7baf56708a01c
Rob Ritter <ritterhaus@gmail.com>
2023-07-11 17:49:31 -0500

    Dinner time commit

--- src/App.js
+++ src/App.js
@@ -7,20 +7,23 @@ import Categories from './components/Categories/Categories';
 import Todos from './components/Todos/Todos';
 import Notfound from './components/NotFound/Notfound';
 import Navigation from './components/Navigation';
+import AuthProvider from './contexts/AuthContext';
 
 export default function App() {
     return (
         <div className='App'>
-            <Router>
-                <Navigation />
-                <Routes>
-                    <Route to='/' element={<Login />} />
-                    <Route to='/login' element={<Login />} />
-                    <Route to='/categories' element={<Categories />} />
-                    <Route to='/todos' element={<Todos />} />
-                    <Route to='/*' element={<Notfound />} />
-                </Routes>
-            </Router>
+            <AuthProvider>
+                <Router>
+                    <Navigation />
+                    <Routes>
+                        <Route to='/' element={<Login />} />
+                        <Route to='/login' element={<Login />} />
+                        <Route to='/categories' element={<Categories />} />
+                        <Route to='/todos' element={<Todos />} />
+                        <Route to='/*' element={<Notfound />} />
+                    </Routes>
+                </Router>
+            </AuthProvider>
         </div>
     );
 }
